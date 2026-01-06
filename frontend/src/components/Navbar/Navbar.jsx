dcfe9ab1456bd0739aaf8f871fa9c491cf6f8b7d
Deepali Katiyar <143311713+Deepali45551@users.noreply.github.com>
2026-01-07 00:35:11 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
 import { FaShoppingCart, FaSearch, FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { StoreContext } from "../../context/Storecontext";
+import { StoreContext } from "../../context/StoreContext.jsx";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ setShowLogin }) => {
